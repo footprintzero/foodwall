526df2571764df4014ce32f3eba0cc27acbd1a61
colinomara37 <colinomara@Colins-MacBook-Pro-2.local>
2019-07-09 15:51:17 +0800

    robot update four

--- robot/model.py
+++ robot/model.py
@@ -5,12 +5,13 @@ import math as m
 robot_parameters = {'num_towers': 183,
                     'operation_hours': 18,
                     'fruit_plant_day': .56,
-                    'robot_cycle_time': 3,
-                    'kw_price': .18}
+                    'robot_rate': 20,
+                    'kw_price': .18,
+                    'indirect_fixed': 100}
 
 working_params = {}
 
-#hi
+
 def update(params=None):
     setup()
     if params is not None:
@@ -28,19 +29,17 @@ result = {'cap': 0,
 
 def run():
     global result
-    result['numR'] = units_needed(working_params['num_towers'],
-                                  working_params['fruit_plant_day'],
-                                  working_params['operation_hours'],
-                                  working_params['robot_cycle_time'])[0]
-    result['numL'] = units_needed(working_params['num_towers'],
-                                  working_params['fruit_plant_day'],
-                                  working_params['operation_hours'],
-                                  working_params['robot_cycle_time'])[1]
+    (numR, numL) = units_needed(working_params['num_towers'],
+                                working_params['fruit_plant_day'],
+                                working_params['operation_hours'],
+                                working_params['robot_rate'])
+    result.update({'numL': numL, 'numR': numR})
     result['cap'] = cap_cost(result['numR'], result['numL'])
     result['op'] = op_cost(result['numR'],
                            result['numL'],
                            working_params['kw_price'],
-                           working_params['operation_hours'])
+                           working_params['operation_hours'],
+                           working_params['indirect_fixed'])
 
 
 def setup():
@@ -48,16 +47,18 @@ def setup():
     working_params = robot_parameters
 
 
-def results():
-    update()
-    return result
+def run_cases(cases):
+    for c in cases:
+        for k in c:
+            working_params[k] = c[k]
+        update()
+        c.update(result)
 
 
-def units_needed(num_towers, fruit_plant_day, operation_hours, robot_cycle_time):
-    # robot cycle time is how many minutes to pick one fruit
-    robot_cycle_time2 = robot_cycle_time/60
-    fruit_hour = num_towers*fruit_plant_day*(12/operation_hours)
-    num_robots_needed = m.ceil(fruit_hour*robot_cycle_time2)
+def units_needed(num_towers, fruit_plant_day, operation_hours, robot_rate):
+    # robot fruit rate = fruits it can pick per hour
+    fruits = num_towers*fruit_plant_day*12
+    num_robots_needed = int(4*(m.ceil((float(fruits/(robot_rate*operation_hours))/4))))
     num_lights_needed = 4*num_robots_needed
     return num_robots_needed, num_lights_needed
 
@@ -77,17 +78,18 @@ def cap_cost(num_robots, num_lights):
     return total
 
 
-def op_cost(num_robots, num_lights, kw_price, operation_hours):
+def op_cost(num_robots, num_lights, kw_price, operation_hours, indirect_fixed):
+    indirect_fixed_total = num_robots*indirect_fixed
     arms_kw = .39
     computer_kw = .51
     scanner_kw = .0025
     stereovision_kw = .004
     lights1_kw = .005
     lights2_kw = .001
-    lights_kw = num_lights*[lights1_kw, lights2_kw]
-    robot_kw = num_robots*[arms_kw, computer_kw, scanner_kw, stereovision_kw]
-    total_kw = sum(lights_kw) + sum(robot_kw)
-    total = total_kw*kw_price*operation_hours*365
+    lights_kw = num_lights*(lights1_kw + lights2_kw)
+    robot_kw = num_robots*(arms_kw + computer_kw + scanner_kw + stereovision_kw)
+    total_kw = lights_kw + robot_kw
+    total = (total_kw*kw_price*operation_hours*365)+indirect_fixed_total
     return total
 
 
