2f9c8b7151d3b727d3632f5212ad6092ee4e2b75
footprintzero <info@footprintzero.com.sg>
2019-07-17 14:00:25 +0800

    updated hvac model and ignored venv using .gitignore

--- hvac/model.py
+++ hvac/model.py
@@ -14,7 +14,6 @@ hvac_parameters = {'duct':
                          'cfan_cfm': 1000,
                          'cfan_velocity': 1,
                          'kwh_per_day': .1,
-                         'natural_ventilation_factor': .4,
                          'type': 'unknown'
                          },
                    'main_unit':
@@ -40,8 +39,8 @@ hvac_parameters = {'duct':
                          'a_humidity_d':70,
                          'a_temp_n': 32,
                          'a_humidity_n': 70,
-                         'length_of_day':12,
-                         'length_of_night':12
+                         'day_hours':12,
+                         'weeks': 40
                          },
                    'capex':{},
                    'opex':{}
@@ -80,12 +79,15 @@ def update(params=None):
     return working_params.copy()
 
 
-def get_supply_return_btu(t_rate, insolence, rf, i_temp, i_humidity, a_temp, a_humidity,
-                          f_hvac, f_nv, num_towers, p_tower, wall_a, roof_a, u, cop, daytime):
+def get_supply_btu(t_rate, insolence, rf, i_temp, i_humidity, a_temp, a_humidity,
+                   f_hvac_cfm, f_nv_cfm, num_towers, p_tower, wall_a, roof_a, u, daytime):
+    air_density = 1.2   # kg/m3
+    f_hvac = f_hvac_cfm*(1/60)*.0283168*air_density  # convert to kg/s
+    f_nv = f_nv_cfm*(1/60)*.0283168*air_density
     h_hat2 = si.state("DBT", a_temp, "RH", a_humidity, 101325)[1]
-    h_hat3 = si.state("DBT", i_temp, "RH", i_humidity, 101325)[1]
-    ah2 = si.state("DBT", a_temp, "RH", a_humidity, 101325)[4]
-    ah3 = si.state("DBT", i_temp, "RH", i_humidity, 101325)[4]
+    h_hat4 = si.state("DBT", i_temp, "RH", i_humidity, 101325)[1]
+    ah_2 = si.state("DBT", a_temp, "RH", a_humidity, 101325)[4]
+    ah_4 = si.state("DBT", i_temp, "RH", i_humidity, 101325)[4]
     if daytime:
         enthalpy_water = 2260
         t_kw = t_rate*(enthalpy_water/60)*num_towers*p_tower
@@ -97,22 +99,23 @@ def get_supply_return_btu(t_rate, insolence, rf, i_temp, i_humidity, a_temp, a_h
         l_kw = 0
         u_kw = (u*(wall_a+(2*roof_a))*(a_temp-i_temp))/1000
         t_water = 0
-    h_return = h_hat3+(t_kw + l_kw + u_kw)/(f_hvac+f_nv)
+    h_hat3 = h_hat4-((t_kw + l_kw + u_kw)/(f_hvac+f_nv))
     h_supply = ((h_hat3*(f_hvac+f_nv))-(f_nv*h_hat2))/f_hvac
-    ah_return = ah3+(t_water/(f_nv+f_hvac))
-    ah_supply = ((ah3*(f_hvac+f_nv))-(f_nv*ah2))/f_hvac
+    ah_3 = ((ah_4*f_hvac)-t_water)/(f_nv+f_hvac)
+    ah_supply = ((ah_3*(f_hvac+f_nv))-(f_nv*ah_2))/f_hvac
 
     supply = si.state("H", h_supply,"W", ah_supply,101325)
     (supply_temp, supply_humidity) = (supply[0], supply[2])
-    sreturn = si.state("H", h_return,"W", ah_return,101325)
-    (return_temp, return_humidity) = (sreturn[0], sreturn[2])
-    max_btu_required = ((f_hvac*(h_return-h_supply))/cop)*3412.142
-    return [supply_temp, supply_humidity, return_temp, return_humidity, max_btu_required]
+    max_btu_required = (f_hvac*(h_hat4-h_supply))*3412.142
+    return [supply_temp, supply_humidity, max_btu_required]
 
 
-def get_total_kwh():
+def get_total_kwh(weeks,cop,max_btu_required,day_hours):
+
     pass
 
+def num_fans():
+    pass
 
 def cap_cost():
     pass
@@ -120,4 +123,5 @@ def cap_cost():
 
 
 def op_cost():
+
     pass
